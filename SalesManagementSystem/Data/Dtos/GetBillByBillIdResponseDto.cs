4e8e57363f6eec4daa68c1c6220433ee41701d01
Abdulrhman Alhemyari <abdulrhman.hazaa@hotmail.com>
2024-01-15 16:42:09 +0300

    update

---
+++ SalesManagementSystem/Data/Dtos/GetBillByBillIdResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SalesManagementSystem.Data.Dtos
+{
+    public class GetBillByBillIdResponseDto
+    {
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
